3f798175c697d9615708931e35591e263065db49
Vaibhav-ks <vaibhavkshashwat2713@gmail.com>
2025-10-09 00:32:13 +0530

    fixed the route

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -79,7 +79,7 @@ app.use((req, res, next) => {
 // --- API Routes ---
 app.use("/api/upload", uploadRouter);
 app.use("/api/staff_issues", staffIssueRoutes);
-app.use("/api/admin_issues", adminIssueRoutes);
+app.use("/api/admin/issues", adminIssueRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/staff", staffRoutes);
 app.use("/api/admin", adminRoutes);
